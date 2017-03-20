8028df7bc882c5b724c8dad5627ceb4c47c70f7d
skim <magiclogy@gmail.com>
2017-03-20 19:49:24 +0800

    New Build Script

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,11 @@
-from distutils.core import setup
 import sys
+from cx_Freeze import setup, Executable
 
-import py2exe
-print("Hello")
-setup(console=[sys.path[0] + "/../ext/CuteR/CuteR/CuteR.py"])
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+setup(  name = "CuteR",
+        version = "0.1",
+        description = "My GUI application!",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable(sys.path[0] + "/../ext/CuteR/CuteR/CuteR.py")])
